88a1026f2fb8c8b4d7ae1e52e4985cda78b24758
esle <essi.lehtola@hotmail.com>
2019-02-11 10:36:34 +0200

    connection to CosmosDB

---
+++ data-storage/AwsConnection-createTable.js
@@ -0,0 +1,30 @@
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+    region: "eu-north-1",
+    endpoint: "http://localhost:8000"
+});
+
+var dynamodb = new AWS.DynamoDB();
+
+var params = {
+    TableName: "SensorData",
+    KeySchema: [
+        { AttributeName: "date", KeyType: "HASH" }
+    ],
+    AttributeDefinitions: [
+        { AttributeName: "date", AttributeType: "N" },
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10
+    }
+};
+
+dynamodb.createTable(params, function (err, data) {
+    if (err) {
+        console.error("Error:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
