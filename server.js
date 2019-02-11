efe2092af11f868c088958a863a1d55214dc00dd
esle <essi.lehtola@hotmail.com>
2019-02-11 13:10:41 +0200

    C:/Program Files/Git/api/data-from-db works

--- server.js
+++ server.js
@@ -5,27 +5,45 @@ const bodyparser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 2550;
 
-app.use(bodyparser.json());
-
 AWS.config.update({
     region: "eu-north-1",
     endpoint: "http://localhost:8000"
 });
-var docClient = new AWS.DynamoDB.DocumentClient();
+const docClient = new AWS.DynamoDB.DocumentClient();
 
+app.use(bodyparser.json());
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get('/api/hello', (req, res) => {
     res.json({ test: 'Hello' });
 });
 
+app.get('/api/data-from-db', (req, res) => {
+    let dataItems = [];
+    let params = {
+        TableName: "SensorData"
+    };
+    console.log("Scanning SensorData table.");
+    docClient.scan(params, onScan);
+
+    function onScan(err, data) {
+        if (err) {
+            console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Scan succeeded.");
+            data.Items.forEach(function (dataItem) {
+                dataItems.push(dataItem);
+            });
+        }
+        res.json(dataItems)
+    }
+});
+
 app.post('/api/send-to-db', (req, res) => {
     dataToBeSaved = req.body;
     console.log(req.body);
 
-    var docClient = new AWS.DynamoDB.DocumentClient();
-
-    var params = {
+    let params = {
         TableName: "SensorData",
         Item: {
             "date": dataToBeSaved.date,
@@ -44,4 +62,4 @@ app.post('/api/send-to-db', (req, res) => {
             console.log("Success, added item:", JSON.stringify(data, null, 2));
         }
     });
-})
+});
